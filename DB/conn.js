38b0da666e9d6826e37220c22dbd9e5c23e94057
venu reddy <venugopalreddy9493@gmail.com>
2023-06-30 18:39:09 +0530

    changes

--- DB/conn.js
+++ DB/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const DB = process.env.DATABASE;
-const DB='mongodb://127.0.0.1:27017/venudb'
+const DB = process.env.DATABASE;
+// const DB='mongodb://127.0.0.1:27017/venudb'
 console.log(DB);
 mongoose.connect(DB).then(() => {
     console.log("connection successful")
